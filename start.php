6f0649da6ab04310c0a2941d31202c97b203c30b
Muhammad Usman <mdusyl@yahoo.com>
2013-02-17 00:09:38 +0600

    Loaded assets in Laravel way, thanks to Javid.

--- start.php
+++ start.php
@@ -7,3 +7,65 @@ Autoloader::namespaces(array(
 Autoloader::map(array(
     'Charisma_Base_Controller' => Bundle::path('charisma').'controllers/base.php',
 ));
+
+Asset::container('header')->bundle('charisma');
+Asset::container('footer')->bundle('charisma');
+
+Asset::container('header')->add('charisma-app', 'css/charisma-app.css');
+Asset::container('header')->add('bootstrap-responsive', 'css/bootstrap-responsive.css');
+Asset::container('header')->add('jquery-ui-1.8.21.custom', 'css/jquery-ui-1.8.21.custom.css');
+Asset::container('header')->add('fullcalendar', 'css/fullcalendar.css');
+Asset::container('header')->add('fullcalendar.print', 'css/fullcalendar.print.css');
+Asset::container('header')->add('chosen', 'css/chosen.css');
+Asset::container('header')->add('uniform.default', 'css/uniform.default.css');
+Asset::container('header')->add('colorbox', 'css/colorbox.css');
+Asset::container('header')->add('jquery.cleditor', 'css/jquery.cleditor.css');
+Asset::container('header')->add('jquery.noty', 'css/jquery.noty.css');
+Asset::container('header')->add('noty_theme_default', 'css/noty_theme_default.css');
+Asset::container('header')->add('elfinder.min', 'css/elfinder.min.css');
+Asset::container('header')->add('elfinder.theme', 'css/elfinder.theme.css');
+Asset::container('header')->add('jquery.iphone.toggle', 'css/jquery.iphone.toggle.css');
+Asset::container('header')->add('opa-icons', 'css/opa-icons.css');
+Asset::container('header')->add('uploadify', 'css/uploadify.css');
+
+
+
+Asset::container('footer')->add('jquery', 'js/jquery-1.7.2.min.js');
+Asset::container('footer')->add('bootstrap-transition', 'js/bootstrap-transition.js');
+Asset::container('footer')->add('bootstrap-alert', 'js/bootstrap-alert.js');
+Asset::container('footer')->add('bootstrap-modal', 'js/bootstrap-modal.js');
+Asset::container('footer')->add('bootstrap-dropdown', 'js/bootstrap-dropdown.js');
+Asset::container('footer')->add('bootstrap-scrollspy', 'js/bootstrap-scrollspy.js');
+Asset::container('footer')->add('bootstrap-tab', 'js/bootstrap-tab.js');
+Asset::container('footer')->add('bootstrap-tooltip', 'js/bootstrap-tooltip.js');
+Asset::container('footer')->add('bootstrap-popover', 'js/bootstrap-popover.js');
+Asset::container('footer')->add('bootstrap-button', 'js/bootstrap-button.js');
+Asset::container('footer')->add('bootstrap-collapse', 'js/bootstrap-collapse.js');
+Asset::container('footer')->add('bootstrap-carousel', 'js/bootstrap-carousel.js');
+Asset::container('footer')->add('bootstrap-typeahead', 'js/bootstrap-typeahead.js');
+Asset::container('footer')->add('bootstrap-tour', 'js/bootstrap-tour.js');
+Asset::container('footer')->add('bootstrap-toggle', 'js/bootstrap-tour.js');
+Asset::container('footer')->add('jquery-ui-1.8.21.custom.min', 'js/jquery-ui-1.8.21.custom.min.js');
+Asset::container('footer')->add('jquery.cookie', 'js/jquery.cookie.js');
+Asset::container('footer')->add('jquery.fullcalendar.min', 'js/fullcalendar.min.js');
+Asset::container('footer')->add('jquery.dataTables.min', 'js/jquery.dataTables.min.js');
+Asset::container('footer')->add('excanvas', 'js/excanvas.js');
+Asset::container('footer')->add('jquery.flot.min', 'js/jquery.flot.min.js');
+Asset::container('footer')->add('jquery.flot.pie.min', 'js/jquery.flot.pie.min.js');
+Asset::container('footer')->add('jquery.flot.stack', 'js/jquery.flot.stack.js');
+Asset::container('footer')->add('jquery.flot.resize.min', 'js/jquery.flot.resize.min.js');
+Asset::container('footer')->add('jquery.chosen.min', 'js/jquery.chosen.min.js');
+Asset::container('footer')->add('jquery.colorbox.min', 'js/jquery.colorbox.min.js');
+Asset::container('footer')->add('jquery.cleditor.min', 'js/jquery.cleditor.min.js');
+Asset::container('footer')->add('jquery.noty', 'js/jquery.noty.js');
+Asset::container('footer')->add('jquery.elfinder.min', 'js/jquery.elfinder.min.js');
+Asset::container('footer')->add('jquery.raty.min', 'js/jquery.raty.min.js');
+Asset::container('footer')->add('jquery.iphone.toggle', 'js/jquery.iphone.toggle.js');
+Asset::container('footer')->add('jquery.autogrow-textarea', 'js/jquery.autogrow-textarea.js');
+Asset::container('footer')->add('jquery.uploadify-3.1.min', 'js/jquery.uploadify-3.1.min.js');
+Asset::container('footer')->add('jquery.history', 'js/jquery.history.js');
+Asset::container('footer')->add('jquery.uniform.min', 'js/jquery.uniform.min.js');
+Asset::container('footer')->add('fullcalendar.min', 'js/fullcalendar.min.js');
+Asset::container('footer')->add('charisma', 'js/charisma.js');
+
+
--- views/layouts/index.php
+++ views/layouts/index.php
@@ -28,22 +28,7 @@
 		padding: 9px 0;
 	  }
 	</style>
-	<link href="css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="css/charisma-app.css" rel="stylesheet">
-	<link href="css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
-	<link href='css/fullcalendar.css' rel='stylesheet'>
-	<link href='css/fullcalendar.print.css' rel='stylesheet'  media='print'>
-	<link href='css/chosen.css' rel='stylesheet'>
-	<link href='css/uniform.default.css' rel='stylesheet'>
-	<link href='css/colorbox.css' rel='stylesheet'>
-	<link href='css/jquery.cleditor.css' rel='stylesheet'>
-	<link href='css/jquery.noty.css' rel='stylesheet'>
-	<link href='css/noty_theme_default.css' rel='stylesheet'>
-	<link href='css/elfinder.min.css' rel='stylesheet'>
-	<link href='css/elfinder.theme.css' rel='stylesheet'>
-	<link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
-	<link href='css/opa-icons.css' rel='stylesheet'>
-	<link href='css/uploadify.css' rel='stylesheet'>
+	<?php echo Asset::container('header')->bundle('charisma')->styles() ?>
 
 	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
@@ -189,75 +174,7 @@
 	================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
 
-	<!-- jQuery -->
-	<script src="js/jquery-1.7.2.min.js"></script>
-	<!-- jQuery UI -->
-	<script src="js/jquery-ui-1.8.21.custom.min.js"></script>
-	<!-- transition / effect library -->
-	<script src="js/bootstrap-transition.js"></script>
-	<!-- alert enhancer library -->
-	<script src="js/bootstrap-alert.js"></script>
-	<!-- modal / dialog library -->
-	<script src="js/bootstrap-modal.js"></script>
-	<!-- custom dropdown library -->
-	<script src="js/bootstrap-dropdown.js"></script>
-	<!-- scrolspy library -->
-	<script src="js/bootstrap-scrollspy.js"></script>
-	<!-- library for creating tabs -->
-	<script src="js/bootstrap-tab.js"></script>
-	<!-- library for advanced tooltip -->
-	<script src="js/bootstrap-tooltip.js"></script>
-	<!-- popover effect library -->
-	<script src="js/bootstrap-popover.js"></script>
-	<!-- button enhancer library -->
-	<script src="js/bootstrap-button.js"></script>
-	<!-- accordion library (optional, not used in demo) -->
-	<script src="js/bootstrap-collapse.js"></script>
-	<!-- carousel slideshow library (optional, not used in demo) -->
-	<script src="js/bootstrap-carousel.js"></script>
-	<!-- autocomplete library -->
-	<script src="js/bootstrap-typeahead.js"></script>
-	<!-- tour library -->
-	<script src="js/bootstrap-tour.js"></script>
-	<!-- library for cookie management -->
-	<script src="js/jquery.cookie.js"></script>
-	<!-- calander plugin -->
-	<script src='js/fullcalendar.min.js'></script>
-	<!-- data table plugin -->
-	<script src='js/jquery.dataTables.min.js'></script>
-
-	<!-- chart libraries start -->
-	<script src="js/excanvas.js"></script>
-	<script src="js/jquery.flot.min.js"></script>
-	<script src="js/jquery.flot.pie.min.js"></script>
-	<script src="js/jquery.flot.stack.js"></script>
-	<script src="js/jquery.flot.resize.min.js"></script>
-	<!-- chart libraries end -->
-
-	<!-- select or dropdown enhancer -->
-	<script src="js/jquery.chosen.min.js"></script>
-	<!-- checkbox, radio, and file input styler -->
-	<script src="js/jquery.uniform.min.js"></script>
-	<!-- plugin for gallery image view -->
-	<script src="js/jquery.colorbox.min.js"></script>
-	<!-- rich text editor library -->
-	<script src="js/jquery.cleditor.min.js"></script>
-	<!-- notification plugin -->
-	<script src="js/jquery.noty.js"></script>
-	<!-- file manager library -->
-	<script src="js/jquery.elfinder.min.js"></script>
-	<!-- star rating plugin -->
-	<script src="js/jquery.raty.min.js"></script>
-	<!-- for iOS style toggle switch -->
-	<script src="js/jquery.iphone.toggle.js"></script>
-	<!-- autogrowing textarea plugin -->
-	<script src="js/jquery.autogrow-textarea.js"></script>
-	<!-- multiple file upload plugin -->
-	<script src="js/jquery.uploadify-3.1.min.js"></script>
-	<!-- history.js for cross-browser state change on ajax -->
-	<script src="js/jquery.history.js"></script>
-	<!-- application script for Charisma demo -->
-	<script src="js/charisma.js"></script>
+	<?php echo Asset::container('footer')->bundle('charisma')->scripts() ?>
 	
 	<?php //Google Analytics code for tracking my demo site, you can remove this.
 		if($_SERVER['HTTP_HOST']=='usman.it') { ?>
