665f5f14ed25fef6e7057506c024294c42b828f5
Muhammad Usman <mdusyl@yahoo.com>
2013-02-16 17:12:32 +0600

    Commenting

--- controllers/home.php
+++ controllers/home.php
@@ -4,7 +4,9 @@ class Charisma_Home_Controller extends Charisma_Base_Controller {
 	
 	public function action_index($view = 'index.html')
 	{
+		// Get rid of `.html` and prepend bundle and folder name
 		$view_str = 'charisma::pages.' . current(explode('.', $view));
+		// Load the view by the given parameter
 		$this->layout->nest('content', $view_str);
 	}
 }
